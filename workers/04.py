aeb9cc3ae0b85bac2c04a415bb4fac20d255fda9
Luiz Gustavo <l19051997@gmail.com>
2026-04-05 18:09:30 -0300

    melhorando logging

--- workers/04.py
+++ workers/04.py
@@ -1,6 +1,7 @@
 import asyncio
 from random import random
 from config import log
+import time
 
 """
 O Problema: Um Producer coloca 10 números em uma asyncio.Queue. Um Consumer retira esses números para processar.
@@ -10,8 +11,8 @@ O Problema: Um Producer coloca 10 números em uma asyncio.Queue. Um Consumer ret
 3.  Seu código deve capturar a exceção de timeout, avisar que o "Item 7 falhou" e continuar processando o restante da fila sem travar o programa.
 """
 
-QUEUE_SIZE = 100
-MAX_CONSUMERS = 10
+QUEUE_SIZE = 10
+MAX_CONSUMERS = 2
 
 
 async def producer(q: asyncio.Queue):
@@ -21,7 +22,6 @@ async def producer(q: asyncio.Queue):
 
 
 async def process(item: dict):
-    log.debug(f"Processando {item['data']} -- tentativa {item['retries']}")
     if item["data"] == 7:
         await asyncio.sleep(5)
     elif item["data"] == 9:
@@ -37,23 +37,25 @@ async def consumer(consumer: str, queue: asyncio.Queue, error_queue: asyncio.Que
             TIMEOUT = 2 ** item["retries"]
             try:
                 async with asyncio.timeout(TIMEOUT):
+                    start = time.perf_counter()
                     await process(item)
-                    log.debug(f"item {item['data']} processado em consumer {consumer}")
+                    log.debug(
+                        f"{consumer} -> {item['data']} processado em {str(time.perf_counter() - start)[:4]}s -- {item['retries']} tentativas"
+                    )
             except asyncio.TimeoutError:
                 item["retries"] += 1
                 if item["retries"] <= 3:
                     await queue.put(item)
                     log.error(
-                        f"erro de timeout ao processar {item['data']} em {consumer}"
+                        f"{consumer} -> {item['data']} timeout -- {item['retries']}"
                     )
                 else:
                     await error_queue.put(item)
-                    log.critical(
-                        f"item {item['data']} falhou definitivamente no consumer {consumer}"
-                    )
+                    log.critical(f"{consumer} -> {item['data']} falhou definitivamente")
             finally:
                 queue.task_done()
     except asyncio.CancelledError:
+        await asyncio.sleep(random())
         log.warning(f"consumer {consumer} encerrado.")
         raise
 
@@ -71,6 +73,7 @@ async def main():
     await error_queue.join()
     await queue.join()
 
+    await asyncio.sleep(1)
     log.info("graceful shutdown dos consumers")
     for t in tasks:
         t.cancel()
@@ -85,6 +88,9 @@ async def main():
 
 
 if __name__ == "__main__":
-    log.info("Teste de producer/consumer iniciado")
+    log.info("Teste de producer/consumer iniciado\n")
+    time.sleep(1)
     asyncio.run(main())
+    time.sleep(1)
+    print("\n")
     log.info("Teste de producer/consumer finalizado")
