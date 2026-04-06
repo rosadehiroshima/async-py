c160eb6a151dcef0337ac1f53b664b5603d5aaf4
Luiz Gustavo <l19051997@gmail.com>
2026-04-06 18:32:58 -0300

    comecando implementacao de sistema pub sub

--- exercicios/workers/04.py
+++ exercicios/workers/04.py
@@ -3,14 +3,6 @@ from random import random
 from config import log
 import time
 
-"""
-O Problema: Um Producer coloca 10 números em uma asyncio.Queue. Um Consumer retira esses números para processar.
-
-1.  O Consumer deve ter um timeout de 2 segundos para cada item.
-2.  Simule que, se o número for "7", o processamento demora 5 segundos (causando um estouro de timeout).
-3.  Seu código deve capturar a exceção de timeout, avisar que o "Item 7 falhou" e continuar processando o restante da fila sem travar o programa.
-"""
-
 QUEUE_SIZE = 10
 MAX_CONSUMERS = 2
 
---
+++ main.py
@@ -0,0 +1,4 @@
+from config import log
+
+if __name__ == "__main__":
+    log.info("Hello, world")
