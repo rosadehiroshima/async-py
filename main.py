139bcf4e326f2bb67d1ca04d1d07abf696688108
Luiz Gustavo <l19051997@gmail.com>
2026-04-13 22:31:05 -0300

    classe de topicos

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+import asyncio
+from typing import Callable, Awaitable, Any
+
 from config import log
+
+# import numpy as np
+# import pandas as pd
 
 """
 DESAFIO: EVOLUÇÃO PARA ARQUITETURA ORIENTADA A EVENTOS (PUB/SUB)
@@ -20,5 +26,38 @@ Etapas de Implementação:
    eventos de compensação (rollback) em caso de falha no fluxo.
 """
 
+
+class Events:
+   _topics: dict[str, set[Callable[..., Awaitable[Any]]]] = {}
+
+   @classmethod
+   def get_topics(cls) -> dict[str, set[Callable[..., Awaitable[Any]]]]:
+      return cls._topics
+
+   @classmethod
+   def get_keys(cls):
+      return cls._topics.keys()
+
+   @classmethod
+   def subscribe(cls, topic: str, callback: Callable[..., Awaitable[Any]]) -> None:
+      if topic not in cls._topics:
+         cls._topics[topic] = set()
+      cls._topics[topic].add(callback)
+
+   @classmethod
+   def unsubscribe(cls, topic: str, callback: Callable[..., Awaitable[Any]]) -> None:
+      if topic in cls._topics:
+         cls._topics[topic].discard(callback)
+
+async def test():
+   log.info("Teste")
+
+async def main():
+   Events.subscribe("teste", test)
+
+   log.info("Tópicos cadastrados nos eventos:")
+   print(Events.get_topics())
+
+
 if __name__ == "__main__":
-    log.info("Hello, world")
+   asyncio.run(main())
